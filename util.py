f564ae08e5ff484169ad4dafa97d0c07273dafda
Omppu42 <51288885+Omppu42@users.noreply.github.com>
2022-11-08 20:51:38 +0200

    timer caller path

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
-import pygame, time, inspect, os
+import time, os
 from block import Block
-pygame.init()
+from functools import wraps
 
 def get_cell_from_mousepos(ui, pos: tuple) -> Block:
     cell = ((pos[0] - ui.total_mouse_change[0]) // ui.cell_size, 
@@ -9,18 +9,18 @@ def get_cell_from_mousepos(ui, pos: tuple) -> Block:
     block = next((x for x in ui.blocks if x.pos_on_grid == cell), None)
     return block
 
+
 def timer(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.time()
         rv = func(*args, **kwargs)
 
-        #get caller filename
-        frame = inspect.stack()[1]
-        module = inspect.getmodule(frame[0])
-        filename = module.__file__
-        filename = os.path.basename(filename)
-
-        print(f"Running the function '{func.__name__}' in '{filename}' took: {time.time() - start_time} seconds")
+        caller_path = os.path.abspath(func.__globals__["__file__"])
+        caller_path = os.path.relpath(caller_path, os.getcwd()) #get path relative to cwd
+        
+        total_time = round(time.time() - start_time, 4)
+        print(f"TIMER: Running the function '{func.__name__}' from file '{caller_path}' took: {total_time} seconds")
         return rv
     
     return wrapper
